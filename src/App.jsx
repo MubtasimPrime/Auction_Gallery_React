3b59bc4c752a9f61113af5f41f95501c60f6e841
Mubtasim-Prime <mubtasimfuadrafiq@gmail.com>
2025-04-09 13:59:57 +0600

    Banner part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
+import Banner from "./components/Banner/Banner";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <div className=" max-w-screen-2xl mx-auto">
+      <div className="max-w-screen-2xl mx-auto">
         <Navbar></Navbar>
       </div>
+      <Banner></Banner>
       <Footer></Footer>
     </>
   );
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="banner-container bg-[url(/banner-img.svg)] bg-cover bg-center min-h-[calc(100vh-72px)] flex items-center">
+      <div className="banner-text text-white max-w-screen-2xl w-full mx-auto">
+        <h1 className="text-5xl font-semibold mb-5">
+          Bid on Unique Items from
+          <br />
+          Around the World
+        </h1>
+        <p className="text-2xl font-light mb-8">
+          Discover rare collectibles, luxury goods, and vintage
+          <br />
+          treasures in our curated auctions
+        </p>
+        <button className="cursor-pointer text-[20px] font-medium bg-white text-black rounded-4xl py-2.5 px-6.5">
+          Explore Auctions
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
