92e8737edc1f47da48b9e0f0d9c1eed3f53d669d
Mubtasim-Prime <mubtasimfuadrafiq@gmail.com>
2025-04-10 15:54:26 +0600

    Toasify added correctly

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Auction from "./components/Auction/Auction";
 import Banner from "./components/Banner/Banner";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const infoJson = fetch("info.json").then((res) => res.json());
   return (
     <>
+      <ToastContainer />
       <div className="max-w-screen-2xl mx-auto">
         <Navbar></Navbar>
       </div>
--- src/components/Single Auction/SingleAuction.jsx
+++ src/components/Single Auction/SingleAuction.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import { GoHeart } from "react-icons/go";
 import { GoHeartFill } from "react-icons/go";
+import { ToastContainer, toast } from "react-toastify";
 
 const SingleAuction = ({ data }) => {
   // console.log(data);
   const [heartColor, setHeartColor] = useState(false);
 
+  // Red Heart Disabled
   const heart = () => setHeartColor(true);
+
+  // Toast
+  const notify = () => toast("An item is added to favorites");
 
   return (
     <>
@@ -23,7 +28,13 @@ const SingleAuction = ({ data }) => {
         <td>{data.currentBidPrice}</td>
         <td>{data.timeLeft}</td>
         <td>
-          <button onClick={heart} disabled={heartColor}>
+          <button
+            onClick={() => {
+              heart();
+              notify();
+            }}
+            disabled={heartColor}
+          >
             {heartColor ? (
               <GoHeartFill className="text-red-500 w-6 h-6 cursor-not-allowed duration-200 hover:scale-110 transition-transform" />
             ) : (
@@ -32,6 +43,7 @@ const SingleAuction = ({ data }) => {
           </button>
         </td>
       </tr>
+      {/* <ToastContainer /> */}
     </>
   );
 };
